2495202873c50ec2351720885fcd0a7e70a0b98b
Sonlanggtu <sonlanggtu@gmail.com>
2020-02-05 15:17:19 +0700

    Init Solution Microservice with RabbitMQ

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabitMQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
